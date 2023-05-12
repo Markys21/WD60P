c66b5740a8c662d1288ba19e413a15d84dd46d23
JOHN MARK ROJAS <126499990+Markys21@users.noreply.github.com>
2023-05-12 23:54:22 +0800

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+// var numberVar = 12;
+// var arrayOfFruits = ["apple","orange","banana"];
+// var nickName = "My name is Markys";
+// var isHuman = true;
+// var bigInNumber = BigInt(5);
+// var nameOfAnimal = null;
+// var nameOfPerson = {firstName: "Markys", LastName:"uwu", age: 19};
+
+// console.log("Number: ", numberVar, typeof numberVar);
+// console.log("String: ", nickName, typeof nickName);
+// console.log("Boolean: ", isHuman, typeof isHuman);
+// console.log("Array: ", arrayOfFruits, typeof arrayOfFruits);
+// console.log("BigInt: ", bigInNumber, typeof bigInNumber);
+// console.log("Null: ", nameOfAnimal, typeof nameOfAnimal);
+// console.log("Object: ", nameOfPerson, typeof nameOfPerson);
+// function getRandomIntInclusive(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min + 1) + min); 
+//   }
+//  getRandomIntInclusive();
+
+const i = [1,2,3,4,5,6]
